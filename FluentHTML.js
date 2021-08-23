ac1529f29e9e8c639c7d27188af9cdba59c868a5
Florian Thiévent <florian.thievent@sbb.ch>
2021-08-23 12:37:15 +0200

    init

---
+++ FluentHTML.js
@@ -0,0 +1,4 @@
+// import {Input} from "./elements/input.js";
+// import {Div} from "./elements/div.js"
+// import p from "./elements/p.js"
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+## fluent-html
+
+Some tests for writing HTML with a JavaScript API in fluent design.
